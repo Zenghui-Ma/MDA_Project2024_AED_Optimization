e65743b3e610707d3a9d5e8e071f3a7c0ff78098
Xiaolong Song <33993813+niko-long@users.noreply.github.com>
2024-05-30 18:37:15 +0200

    update aed location marker function file

--- aed_location_existed.py
+++ aed_location_existed.py
@@ -1,6 +1,7 @@
 import dash_leaflet as dl
 import pandas as pd
 import os
+import dash
 
 # 读取AED位置数据
 def read_aed_data(file_path='data/AED_locations.xlsx'):
@@ -22,6 +23,45 @@ def generate_aed_markers(data):
             position=[row['latitude'], row['longitude']],
             icon={
                 "iconUrl": "/assets/aed_old.png",  # 确保aed_icon.png文件位于assets文件夹中
+                "iconSize": [21, 21],  # 图标大小
+                "iconAnchor": [12, 41],  # 图标锚点
+                "popupAnchor": [1, -34],  # 弹出框锚点
+            }
+        )
+        markers.append(marker)
+    return markers
+
+def read_patient_data(file_path='data/citycombined_intervention_data.xlsx'):
+    # 获取当前工作目录
+    base_dir = os.getcwd()
+    # 拼接相对路径，得到文件的绝对路径
+    full_path = os.path.join(base_dir, file_path)
+    # 读取Excel数据
+    data = pd.read_excel(full_path)
+    # 确保数据中没有空值
+    data = data.dropna(subset=['latitude', 'longitude'])
+    return data
+
+
+# display patients location
+import dash_leaflet as dl
+
+def generate_patient_markers(data):
+    markers = []
+    for _, row in data.iterrows():
+        # 根据 target 列的值选择图标颜色
+        if row['target'] == 0:
+            icon_url = "/assets/green_person.png"  # 绿色圈的图标文件路径
+        elif row['target'] == 1:
+            icon_url = "/assets/red_person.png"  # 红色圈的图标文件路径
+        else:
+            continue  # 如果 target 不是 0 或 1，则跳过该行
+
+        # 创建标记
+        marker = dl.Marker(
+            position=[row['latitude'], row['longitude']],
+            icon={
+                "iconUrl": icon_url,
                 "iconSize": [21, 21],  # 图标大小
                 "iconAnchor": [12, 41],  # 图标锚点
                 "popupAnchor": [1, -34],  # 弹出框锚点
