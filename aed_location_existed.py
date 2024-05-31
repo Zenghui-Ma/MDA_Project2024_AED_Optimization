e9853b1eec2f5abc3d77ce782be82e9caf8e53bf
Xiaolong Song <33993813+niko-long@users.noreply.github.com>
2024-05-31 19:14:54 +0200

    stable version 0.1.0

--- aed_location_existed.py
+++ aed_location_existed.py
@@ -3,56 +3,66 @@ import pandas as pd
 import os
 import dash
 
-# 读取AED位置数据
+###############################
+#   AED location and marker   #
+###############################
+
 def read_aed_data(file_path='data/AED_locations_ThreeCity.xlsx'):
-    # 获取当前工作目录
+    # Get the current working directory
     base_dir = os.getcwd()
-    # 拼接相对路径，得到文件的绝对路径
+    # Combine relative path to get the full file path
     full_path = os.path.join(base_dir, file_path)
-    # 读取Excel数据
+    # Read the Excel data
     data = pd.read_excel(full_path)
-    # 确保数据中没有空值
+    # Ensure there are no null values in the data
     data = data.dropna(subset=['latitude', 'longitude'])
     return data
 
-# 生成AED标记
+# Generate AED markers
 def generate_aed_markers(data):
     markers = []
     for _, row in data.iterrows():
         marker = dl.Marker(
             position=[row['latitude'], row['longitude']],
             icon={
-                "iconUrl": "/assets/aed_old.png",  # 确保aed_icon.png文件位于assets文件夹中
-                "iconSize": [21, 21],  # 图标大小
-                "iconAnchor": [12, 41],  # 图标锚点
-                "popupAnchor": [1, -34],  # 弹出框锚点
+                "iconUrl": "/assets/aed_old.png",  # Ensure aed_icon.png is located in the assets folder
+                "iconSize": [21, 21],  # Icon size
+                "iconAnchor": [12, 41],  # Icon anchor point
+                "popupAnchor": [1, -34],  # Popup anchor point
             }
         )
         markers.append(marker)
     return markers
 
+<<<<<<< Updated upstream
 <<<<<<< HEAD
 def read_patient_data(file_path='data/citycombined_intervention_data.xlsx'):
 =======
 def read_patient_data(file_path='data/patients_combined.xlsx'):
 >>>>>>> a77a5a4788b8c0485012dea46a31ca23c9464aeb
     # 获取当前工作目录
+=======
+###############################
+# patient location and marker #
+###############################
+
+def read_patient_data(file_path='data/patients_combined.xlsx'):
+    # Get the current working directory
+>>>>>>> Stashed changes
     base_dir = os.getcwd()
-    # 拼接相对路径，得到文件的绝对路径
+    # Combine relative path to get the full file path
     full_path = os.path.join(base_dir, file_path)
-    # 读取Excel数据
+    # Read the Excel data
     data = pd.read_excel(full_path)
-    # 确保数据中没有空值
+    # Ensure there are no null values in the data
     data = data.dropna(subset=['latitude', 'longitude'])
     return data
 
-
-# display patients location
-import dash_leaflet as dl
-
+# Generate patient markers
 def generate_patient_markers(data):
     markers = []
     for _, row in data.iterrows():
+<<<<<<< Updated upstream
         # 根据 target 列的值选择图标颜色
 <<<<<<< HEAD
         if row['target'] == 0:
@@ -62,22 +72,61 @@ def generate_patient_markers(data):
         else:
             continue  # 如果 target 不是 0 或 1，则跳过该行
 =======
+=======
+        # Select icon color based on the target column value
+>>>>>>> Stashed changes
         if row['Mortality'] == 0:
-            icon_url = "/assets/green_person.png"  # 绿色圈的图标文件路径
+            icon_url = "/assets/green_person.png"  # Path to green icon
         elif row['Mortality'] == 1:
-            icon_url = "/assets/red_person.png"  # 红色圈的图标文件路径
+            icon_url = "/assets/red_person.png"  # Path to red icon
         else:
+<<<<<<< Updated upstream
             continue  # 如果 Mortality 不是 0 或 1，则跳过该行
 >>>>>>> a77a5a4788b8c0485012dea46a31ca23c9464aeb
+=======
+            continue  # Skip if Mortality is not 0 or 1
+>>>>>>> Stashed changes
 
-        # 创建标记
+        # Create marker
         marker = dl.Marker(
             position=[row['latitude'], row['longitude']],
             icon={
                 "iconUrl": icon_url,
-                "iconSize": [21, 21],  # 图标大小
-                "iconAnchor": [12, 41],  # 图标锚点
-                "popupAnchor": [1, -34],  # 弹出框锚点
+                "iconSize": [21, 21],  # Icon size
+                "iconAnchor": [12, 41],  # Icon anchor point
+                "popupAnchor": [1, -34],  # Popup anchor point
+            }
+        )
+        markers.append(marker)
+    return markers
+
+###############################
+## hospital location marker  ##
+###############################
+
+# Read hospital location data
+def read_hospital_data(file_path='data/hospitals.xlsx'):
+    # Get the current working directory
+    base_dir = os.getcwd()
+    # Combine relative path to get the full file path
+    full_path = os.path.join(base_dir, file_path)
+    # Read the Excel data
+    data = pd.read_excel(full_path)
+    # Ensure there are no null values in the data
+    data = data.dropna(subset=['latitude', 'longitude'])
+    return data
+
+# Generate hospital markers
+def generate_hospital_markers(data):
+    markers = []
+    for _, row in data.iterrows():
+        marker = dl.Marker(
+            position=[row['latitude'], row['longitude']],
+            icon={
+                "iconUrl": "/assets/hospital.png",  # Ensure hospital.png is located in the assets folder
+                "iconSize": [21, 21],  # Icon size
+                "iconAnchor": [12, 41],  # Icon anchor point
+                "popupAnchor": [1, -34],  # Popup anchor point
             }
         )
         markers.append(marker)
