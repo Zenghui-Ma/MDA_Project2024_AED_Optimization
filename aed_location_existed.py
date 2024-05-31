8824756824c188c541d60ac45ffe5a56b5f78936
Xiaolong Song <33993813+niko-long@users.noreply.github.com>
2024-05-31 19:48:52 +0200

    fix aed_location_existed file

--- aed_location_existed.py
+++ aed_location_existed.py
@@ -34,21 +34,12 @@ def generate_aed_markers(data):
         markers.append(marker)
     return markers
 
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-def read_patient_data(file_path='data/citycombined_intervention_data.xlsx'):
-=======
-def read_patient_data(file_path='data/patients_combined.xlsx'):
->>>>>>> a77a5a4788b8c0485012dea46a31ca23c9464aeb
-    # 获取当前工作目录
-=======
 ###############################
 # patient location and marker #
 ###############################
 
 def read_patient_data(file_path='data/patients_combined.xlsx'):
     # Get the current working directory
->>>>>>> Stashed changes
     base_dir = os.getcwd()
     # Combine relative path to get the full file path
     full_path = os.path.join(base_dir, file_path)
@@ -62,30 +53,13 @@ def read_patient_data(file_path='data/patients_combined.xlsx'):
 def generate_patient_markers(data):
     markers = []
     for _, row in data.iterrows():
-<<<<<<< Updated upstream
-        # 根据 target 列的值选择图标颜色
-<<<<<<< HEAD
-        if row['target'] == 0:
-            icon_url = "/assets/green_person.png"  # 绿色圈的图标文件路径
-        elif row['target'] == 1:
-            icon_url = "/assets/red_person.png"  # 红色圈的图标文件路径
-        else:
-            continue  # 如果 target 不是 0 或 1，则跳过该行
-=======
-=======
         # Select icon color based on the target column value
->>>>>>> Stashed changes
         if row['Mortality'] == 0:
             icon_url = "/assets/green_person.png"  # Path to green icon
         elif row['Mortality'] == 1:
             icon_url = "/assets/red_person.png"  # Path to red icon
         else:
-<<<<<<< Updated upstream
-            continue  # 如果 Mortality 不是 0 或 1，则跳过该行
->>>>>>> a77a5a4788b8c0485012dea46a31ca23c9464aeb
-=======
             continue  # Skip if Mortality is not 0 or 1
->>>>>>> Stashed changes
 
         # Create marker
         marker = dl.Marker(
